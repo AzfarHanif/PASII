5d3f927fa10babb751697636101094f09498e99a
AzfarHanif <azfarhanifamulya@gmail.com>
2025-05-21 06:03:09 +0700

    add dark-mode and footer

--- script.js
+++ script.js
@@ -1,18 +1,12 @@
 const icon = document.getElementById('toggle-icon');
 
-document.getElementById('toggle-dark-mode').addEventListener('click', function () {
+document.getElementById('toggle-dark-mode').addEventListener('change', function () {
   document.body.classList.toggle('dark-mode');
-  const nav = document.getElementById('mainNavbar');
-  nav.classList.toggle('navbar-light');
-  nav.classList.toggle('navbar-dark');
-  nav.classList.toggle('bg-light');
-  nav.classList.toggle('bg-dark');
 
-  // Ganti gambar ikon
-  if (document.body.classList.contains('dark-mode')) {
-    icon.src = 'assets/icons/dark-mode.png';
-  } else {
-    icon.src = 'assets/icons/light_mode.png';
+  const nav = document.getElementById('mainSection');
+  if (nav) {
+    // nav.classList.toggle('bg-light');
+    // nav.classList.toggle('bg-dark');
   }
 });
 
