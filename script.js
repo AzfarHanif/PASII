906e96fbfeee2ed7d5bf5003c852ea70e0389083
AzfarHanif <azfarhanifamulya@gmail.com>
2025-05-21 22:04:42 +0700

    animation

--- script.js
+++ script.js
@@ -10,6 +10,24 @@ document.getElementById('toggle-dark-mode').addEventListener('change', function 
   }
 });
 
+document.addEventListener("DOMContentLoaded", () => {
+    const cards = document.querySelectorAll(".card");
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("animate");
+            }
+        });
+    }, {
+        threshold: 0.1
+    });
+
+    cards.forEach(card => {
+        observer.observe(card);
+    });
+});
+
 document.querySelector('.trigger-btn').addEventListener('click', showPopup);
 
 function showPopup() {
